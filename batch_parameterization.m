f99d68291dc3b2a82466e614d3336a40253e6cea
charlesdu <Charles Du>
2019-10-03 16:43:03 -0500

    batch experiment based on selected file list

--- batch_parameterization.m
+++ batch_parameterization.m
@@ -7,38 +7,45 @@ rng(1)
 clear
 addpath('mex');
 
-resDir = '~/data/D1_LBD_result';
-fileList = dir('~/data/D1_LBD_dataset');
+datasetDir = '~/data/D1_LBD_dataset';
+resDir = '~/data/D1_LBD_largeK_result';
+fileNameList = readtable('~/MEGAsync/D1/LBD/D1NameList.csv','ReadVariableNames',false);
+fileNameList = fileNameList.(1);
 
-for file = fileList.'
-    if ~file.isdir()
-        disp(file.name);
-        ffile = fullfile(file.folder, file.name);
-        
+diary '~/MEGAsync/D1/LBD/batch_largeK_info.txt'
+
+for filebasename = fileNameList.'
+    filename = join([filebasename,'.mat'],'');
+    filename = filename{1};
+    disp(filename);
+    ffile = fullfile(datasetDir, filename);
+    
+%     break;
         
 %% load data: V, F, x0, hdls, K
-        load(ffile);
+    load(ffile);
 
 %% parameters
-        K = K * 2;
-        lb = -1; % lower bound on SVs (-1 = disabled)
-        ub = -1; % upper bound on SVs (-1 = disabled)
-        iter_max = 1000; % maximal number of BD projection iterations
-        tol_err = 1e-10; % tolerance for stopping BD projection iterations
-        use_weighted_metric = false; % use a weighted metric?
+%     K = K * 2;
+    K = 2 * 8.57931e6;
+    lb = -1; % lower bound on SVs (-1 = disabled)
+    ub = -1; % upper bound on SVs (-1 = disabled)
+    iter_max = 100000; % maximal number of BD projection iterations
+    tol_err = 1e-10; % tolerance for stopping BD projection iterations
+    use_weighted_metric = false; % use a weighted metric?
 
 %% generate problem
 
 % some constants
-        dim = size(F,2)-1;
-        n_vert = size(V,1);
-        n_tri = size(F,1);
+    dim = size(F,2)-1;
+    n_vert = size(V,1);
+    n_tri = size(F,1);
 
 % setup linear constraints (fixed handles)
-        n_hdls = size(hdls,2);
-        sp = sparse(1:n_hdls,hdls,1,n_hdls,n_vert);
-        eq_lhs = kron(eye(dim),sp);
-        eq_rhs = eq_lhs*colStack(x0);
+    n_hdls = size(hdls,2);
+    sp = sparse(1:n_hdls,hdls,1,n_hdls,n_vert);
+    eq_lhs = kron(eye(dim),sp);
+    eq_rhs = eq_lhs*colStack(x0);
 
 % plot surface
 % figure;
@@ -51,7 +58,7 @@ for file = fileList.'
 
 %% solve problem
 % setup BD solver
-        solver_bd = SolverProjectorBD(F, V, eq_lhs, eq_rhs, K, lb, ub, x0, SolverProjectorModeEnum.Tangent, use_weighted_metric);
+    solver_bd = SolverProjectorBD(F, V, eq_lhs, eq_rhs, K, lb, ub, x0, SolverProjectorModeEnum.Tangent, use_weighted_metric);
 
 % plot initial map
 % figure;
@@ -60,7 +67,7 @@ for file = fileList.'
 % hA(1) = gca;
 
 % run solver
-        solver_bd.solve(iter_max, tol_err); % solve BD projection
+    solver_bd.solve(iter_max, tol_err); % solve BD projection
 
 % plot output map
 % figure;
@@ -71,7 +78,8 @@ for file = fileList.'
 % linkaxes(hA);
 
 % save result
-        y = solver_bd.y;
-        save(fullfile(resDir,file.name),'y','F');
-    end
+    y = solver_bd.y;
+    save(fullfile(resDir,filename),'y','F');
 end
+
+diary off;
